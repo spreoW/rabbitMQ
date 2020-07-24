423e565cc753badd1df254e50c2495e37e8918f3
w <wqqw1998@163.com>
2020-07-24 10:04:35 +0800

    7-24

---
+++ src/main/java/com/rabbitmq/api/work/Recv1.java
@@ -0,0 +1,42 @@
+package com.rabbitmq.api.work;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * @author wq
+ * 接收消息
+ */
+public class Recv1 {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.80.5");
+        factory.setUsername("wq");
+        factory.setPassword("wq");
+        factory.setVirtualHost("/wq");
+        Connection connection = factory.newConnection();
+        // 创建通道
+        Channel channel = connection.createChannel();
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        /**
+         *  channel.basicQos(1); 一次不要给工人一个以上的消息.
+         * 不要向工作人员发送新消息，直到它处理并确认了前一个消息。 相反，它会将其分派给不是仍然忙碌的下一个工作人员。
+         */
+        channel.basicQos(1);
+        // 定义队列的消费者
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            // 获取消息，并且处理，这个方法类似事件监听，如果有消息的时候，会被自动调用
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        // 监听队列，第二个参数：是否自动进行消息确认。
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
